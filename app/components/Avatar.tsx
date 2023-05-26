a6b094d8780472271ca5d7a8d6b287c521c38b13
david emioma <davidemiomauche@gmail.com>
2023-05-26 20:14:52 +0100

    Changed next version from 13.4.4 to 13.4.3

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -11,13 +11,17 @@ interface Props {
 
 const Avatar = ({ user }: Props) => {
   return (
-    <div className="relative w-9 h-9 md:w-11 md:h-11 rounded-full overflow-hidden">
-      <Image
-        className="object-cover"
-        src={user?.image || "/assets/placeholder.jpeg"}
-        fill
-        alt="profile"
-      />
+    <div className="relative">
+      <div className="relative w-9 h-9 md:w-11 md:h-11 rounded-full overflow-hidden">
+        <Image
+          className="object-cover"
+          src={user?.image || "/assets/placeholder.jpeg"}
+          fill
+          alt="profile"
+        />
+      </div>
+
+      <div className="absolute top-0 right-0 bg-green-500 w-2 h-2 md:h-3 md:w-3 rounded-full ring-2 ring-white" />
     </div>
   );
 };
